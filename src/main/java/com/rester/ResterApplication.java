33ffe49ce52366a4926394caf918d1f1fd16827c
xinyu <xinyu.29@qq.com>
2021-04-29 09:47:54 +0800

    跨域：设置过滤器

--- src/main/java/com/rester/ResterApplication.java
+++ src/main/java/com/rester/ResterApplication.java
@@ -2,9 +2,24 @@ package com.rester;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class ResterApplication {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ResterApplication.class, args);
