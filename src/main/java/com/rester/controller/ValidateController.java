666cc72f4d0b1b948b3eaf50b86d89167af8eb05
xinyu <xinyu.29@qq.com>
2021-04-29 09:48:19 +0800

    优化

--- src/main/java/com/rester/controller/ValidateController.java
+++ src/main/java/com/rester/controller/ValidateController.java
@@ -21,8 +21,8 @@ import java.util.Map;
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class ValidateController {
-    @PostMapping(path ="/restapi")
-    public Map valiByContext(String category,String context){
+    @PostMapping(path ="/restapi-text")
+    public Map valiByContext(@RequestParam("category") String category,@RequestParam("context") String context){
         System.out.println(category);
         ValidatorController validator = new ValidatorController();
         String categoryResult[]=null;
@@ -42,44 +42,56 @@ public class ValidateController {
     }
 
     @PostMapping(path ="/restapi-file")
-    public Map valiByFile(String category, @RequestParam("testfile") MultipartFile file) throws IOException {
-        InputStream in = file.getInputStream();
+    public Map valiByFile(String category, @RequestParam("testfile") MultipartFile file)  {
+        System.out.println("start file validate");
+        InputStream in = null;
         List<Byte> b = new ArrayList<Byte>();
-        //byte b[] = new byte[10240];
         int len = 0;
         int temp = 0;          //所有读取的内容都使用temp接收
-        while ((temp = in.read()) != -1) {    //当没有读取完时，继续读取
-            //b[len]=(byte)temp;
-            b.add((byte) temp);
-            len++;
+        try {
+            in = file.getInputStream();
+            //byte b[] = new byte[10240];
+            while ((temp = in.read()) != -1) {    //当没有读取完时，继续读取
+                //b[len]=(byte)temp;
+                b.add((byte) temp);
+                len++;
+            }
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         byte[] bb = Bytes.toArray(b);
         String context = new String(bb, 0, len);
+
         //System.out.println(context);
-        in.close();
-        System.out.println(context);
         ValidatorController validator = new ValidatorController();
         String categoryResult[]=null;
         if(category!=null){
             categoryResult= ConfigManager.getInstance().getValue(category.toUpperCase()).split(",",-1);
-            System.out.println(categoryResult);
+            System.out.println("cateresult:"+categoryResult);
         }
 
         Map<String, Object> result=null;
         if(context!=null){
+            System.out.println("start RAD validate");
             validator.validateByString(new RequestContext(), context);
             result=validator.getValidateResult();
         }
+        System.out.println("name:"+result.get("name"));
         result.put("categoryResult",categoryResult);
         return result;
 
     }
     @PostMapping(path ="/restapi-url")
-    public Map valiByUrl(String category, String url) throws IOException {
+    public Map valiByUrl(String category, String url) {
         ValidatorController validator = new ValidatorController();
-        String context =validator.getUrlContents(url, false, false); //获取url提供的swagger文档，返回响应entity
+        String context = null; //获取url提供的swagger文档，返回响应entity
+        try {
+            context = validator.getUrlContents(url, false, false);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         //System.out.println(context);
-        System.out.println(context);
 
         String categoryResult[]=null;
         if(category!=null){
@@ -89,8 +101,10 @@ public class ValidateController {
 
         Map<String, Object> result=null;
         if(context!=null){
+            System.out.println("start url evaluate");
             validator.validateByString(new RequestContext(), context);
             result=validator.getValidateResult();
+            System.out.println("name:"+result.get("name"));
         }
         result.put("categoryResult",categoryResult);
         return result;
